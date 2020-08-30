12d8b38375cd1d90c0d50dea336a7bb94d16e266
Patrick Soethe <dont@know.me>
2020-08-30 11:29:46 +0200

    REST Tab implementiert

--- Screens/RESTScreen.js
+++ Screens/RESTScreen.js
@@ -5,11 +5,33 @@ import { ActivityIndicator, FlatList, Text, View, StyleSheet } from 'react-nativ
 export default function RESTScreen() {
 
 
+  const [isLoading, setLoading] = useState(true);
+  const [data, setData] = useState([]);
+
+
+
+
+  useEffect(() => {
+      fetch('https://restapitestpat.azurewebsites.net/api/persons')      
+      .then((response) => response.json())
+      .then((json) => setData(json))
+      .catch((error) => console.error(error))
+      .finally(() => setLoading(false));
+  }, []);
   
-    return (
-      <View style={{ flex: 1, padding: 24, marginTop: 30 }}>
-        
-      </View>
-    );
+
+  return (
+    <View style={{ flex: 1, padding: 24, marginTop: 30 }}>
+      {isLoading ? <ActivityIndicator/> : (
+        <FlatList
+          data={data}
+          keyExtractor={({ id }, index) => id.toString()}
+          renderItem={({ item }) => (
+            <Text>{item.firstName} {item.lastName}</Text>
+          )}
+        />
+      )}
+    </View>
+  );
 
 }
