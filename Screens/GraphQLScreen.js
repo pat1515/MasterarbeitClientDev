11d77d5458816a7a256984aed8342a315503609d
Patrick Soethe <dont@know.me>
2020-09-06 20:46:02 +0200

    GraphQLScreen - Daten holen in Funktion ausgelagert

--- Screens/GraphQLScreen.js
+++ Screens/GraphQLScreen.js
@@ -1,13 +1,15 @@
 import React, { useEffect, useState } from 'react';
-import { ActivityIndicator, FlatList, Text, View, StyleSheet } from 'react-native';
+import { ActivityIndicator, FlatList, Text, View, StyleSheet, Button, Alert } from 'react-native';
 
 
 export default function GraphQLScreen() {
 
-    const [isLoading, setLoading] = useState(true);
+    
+    const [isLoading, setLoading] = useState(false);
     const [data, setData] = useState([]);
 
 
+    // Abfrage
     const queryString = `
         {
             authors {
@@ -18,8 +20,15 @@ export default function GraphQLScreen() {
     `;
 
 
-    useEffect(() => {
-        fetch('https://graphqlpat.azurewebsites.net/graphql', {
+    // Daten holen GRAPHQL
+    function holeDaten() {
+      
+      // Initialisieren
+      setLoading(true);
+      setData([]);
+
+      // HTTP Request durchführen
+      fetch('https://graphqlpat.azurewebsites.net/graphql', {
             method: 'POST',
             headers: {
                 Accept: 'application/json',
@@ -33,17 +42,33 @@ export default function GraphQLScreen() {
         .then((json) => setData(json.data.authors))
         .catch((error) => console.error(error))
         .finally(() => setLoading(false));
-    }, []);
+    };
     
   
+    // User Interface
     return (
+      
       <View style={{ flex: 1, padding: 24, marginTop: 30 }}>
+        
+        <Button 
+          title="Daten holen"
+          onPress={() => {
+            const start = Date.now();           
+            holeDaten();
+            const ende = Date.now();
+
+            const millis = {ende}.ende - {start}.start;
+            
+            Alert.alert('Laufzeit', {millis}.millis + " ms");
+          }} 
+        />
+
         {isLoading ? <ActivityIndicator/> : (
           <FlatList
             data={data}
             keyExtractor={({ id }, index) => id.toString()}
             renderItem={({ item }) => (
-              <Text>{item.name}</Text>
+              <Text style={styles.itemStyle}>{item.name}</Text>
             )}
           />
         )}
@@ -51,3 +76,18 @@ export default function GraphQLScreen() {
     );
 
 }
+
+
+
+const styles = StyleSheet.create({
+  itemStyle: {
+    flex: 1,
+    marginBottom: 3,
+    //backgroundColor: '#fff',
+    //alignItems: 'center',
+    //justifyContent: 'center',
+    
+    fontSize: 12
+
+  },
+});
